b5595b742a25d74dd731c0c505d11a2763618f30
Admin <https://github.com/coleman2246/JCord.git>
2020-03-16 16:41:52 -0400

    Displays primary stage

--- Client/src/main/java/Channels/display.java
+++ Client/src/main/java/Channels/display.java
@@ -2,11 +2,25 @@ package Channels;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class display extends Application {
     @Override // Override the start method in the Application class
     public void start(Stage primaryStage) {
+        // Creating the border pane
+        BorderPane pane_frame = new BorderPane();
+
+        // Allowing the nodes inside the pane
+        //pane_frame.setTop(getHBox());
+        //pane_frame.setBottom(getVBox());
+
+        Scene display_scene = new Scene(pane_frame);//placing pane in scene
+        primaryStage.setTitle("Display Channels");// setting stage title
+        primaryStage.setScene(display_scene);// placing scene into the stage
+        primaryStage.show();// displaying stage
+
     }
 
     /**
