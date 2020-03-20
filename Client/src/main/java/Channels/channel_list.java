22ad241067275c08831aca961414042224903e00
Admin <https://github.com/coleman2246/JCord.git>
2020-03-20 15:04:49 -0400

    Arrow label added

--- Client/src/main/java/Channels/channel_list.java
+++ Client/src/main/java/Channels/channel_list.java
@@ -2,6 +2,10 @@ package Channels;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -13,16 +17,51 @@ public class channel_list extends Application {
 
     @Override
     public void start(Stage stage) throws Exception{
+        // stage requirements
+
         stage.setTitle("Display channels");
-        stage.setWidth(350);
-        stage.setWidth(500);
-
+        stage.setWidth(310);
+        stage.setHeight(650);
+        // initializing Vbox
         VBox root = new VBox();
-        Label label1 = new Label("CHANNEL LIST");
+        ImageView arrow = new ImageView("red.png");
+        // label
+        /*
+        * ImageView iv2 = new ImageView();
+        iv2.setImage(hashtag);
+        iv2.setFitWidth(25);
+        iv2.setPreserveRatio(true);
+        iv2.setSmooth(true);
+        iv2.setCache(true);
+        */
+        Label label1 = new Label("CHANNEL LIST", arrow);
         label1.setId("customize_label");
-        root.getChildren().addAll(label1);
 
-        Scene scene = new Scene(root,200,600);
+        // initializing the image
+        Image hashtag = new Image("hastag.png");
+        // Button 1
+        ImageView iv2 = new ImageView();
+        iv2.setImage(hashtag);
+        iv2.setFitWidth(25);
+        iv2.setPreserveRatio(true);
+        iv2.setSmooth(true);
+        iv2.setCache(true);
+
+        //Button 2
+        ImageView iv3 = new ImageView();
+        iv3.setImage(hashtag);
+        iv3.setFitWidth(25);
+        iv3.setPreserveRatio(true);
+        iv3.setSmooth(true);
+        iv3.setCache(true);
+
+        Button button = new Button("Chat Channel", iv2);
+        Button button2 = new Button("Voice Chat Channel", iv3);
+        root.getChildren().addAll(label1);
+        root.getChildren().addAll(button,button2);
+
+
+        Scene scene = new Scene(root,300,500);
         scene.getStylesheets().add("customCss.css");
         stage.setScene(scene);
         stage.show();
