48304b1abe13b6072158fb19196978e0d2c1c2c3
Cole <cole2247@gmail.com>
2020-03-24 04:02:57 -0400

    Have a basic create account/login setup

--- Client/src/main/java/Jcord/Login.java
+++ Client/src/main/java/Jcord/Login.java
@@ -1,16 +1,22 @@
 package Jcord;
 
-import java.awt.Color;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.MalformedURLException;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -21,9 +27,52 @@ public class Login extends Application {
     Stage stage;
     Image profilePicture;
     GridPane pane = new GridPane();
+    User user;
 
-    boolean signedIn() {
-        return false;
+    boolean isSignedIn() {
+        return new File("/resources/profile.user").exists();
+    }
+
+    User getUser() {
+        return this.user;
+    }
+
+    User getUserFromFile() throws IOException {
+        File newUserFile = new File("src/main/resources/UserInfo/profile.user");
+        FileInputStream fileInStream = new FileInputStream(newUserFile);
+        ObjectInputStream objectStream = new ObjectInputStream(fileInStream);
+
+
+        try {
+            user = (User) objectStream.readObject();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+
+    void saveUser() {
+        FileOutputStream fileStream;
+		try {
+            File newUserFile = new File("src/main/resources/UserInfo/profile.user");
+            newUserFile.createNewFile();
+
+            fileStream = new FileOutputStream(newUserFile);
+            ObjectOutputStream objectStream = new ObjectOutputStream(fileStream);
+            objectStream.writeObject(this.user);
+            
+            
+            objectStream.close();
+            fileStream.close();
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e){
+            e.printStackTrace();
+
+        }
     }
 
     ClickableButton makeImageCircle(Image image) {
@@ -48,22 +97,46 @@ public class Login extends Application {
 
         TextField userNameField = new TextField();
         userNameField.getStyleClass().add("login");
+        userNameField.setPromptText("Username");
+
+  
+
 
         Text userName = new Text("Username: ");
         userName.setFont(new Font("Whitney", 12));
         userName.setFill(javafx.scene.paint.Color.WHITE);
 
-        pane.add(userName, 0, 0);
-        pane.add(userNameField, 3, 0);
+        //pane.add(userName, 1, 0);
+        pane.add(userNameField, 1, 0);
 
         this.profilePicture = new Image("Images/default_pp.jpg");
 
-        pane.add(makeImageCircle(profilePicture), 2, 1);
+        pane.add(makeImageCircle(profilePicture), 0,0);
 
+        pane.getStyleClass().add("login");
+
+        Button create = new Button("Create Account");
+        
+        create.setOnAction( e->{
+            user = new User(userNameField.getText(), this.profilePicture);
+            saveUser();
+        });
+
+
+        pane.add(create,1,1);
         pane.getStyleClass().add("login");
 
         Scene scene = new Scene(pane);
         scene.getStylesheets().add("customCss.css");
+
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                create.requestFocus();
+            }
+        });
+        
+
         return scene;
     }
 
@@ -80,7 +153,7 @@ public class Login extends Application {
                 File pfp = newPfp.showOpenDialog(stage);
                 try {
                     profilePicture = new Image(pfp.toURI().toURL().toString());
-                    pane.add(makeImageCircle(profilePicture), 2, 1);
+                    pane.add(makeImageCircle(profilePicture), 0, 0);
 
                 } catch (MalformedURLException e1) {
                     // TODO Auto-generated catch block
